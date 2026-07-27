e0542f106b6eba64a0ff6898f46f16c2e05f0f6f
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-28 03:30:45 +0530

    student screen structure and header created

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -1,4 +1,47 @@
 import streamlit as st
+from src.components.footer import footer_dashboard
+
+from src.ui.base_layout import (
+    style_background_dashboard,
+    style_base_layout,
+    style_compact_top_spacing,
+)
+from src.components.header import header_dashboard
+
+
 
 def student_screen():
-    st.header("Student Screen") 
+
+    style_background_dashboard()
+    style_base_layout()
+    style_compact_top_spacing()
+
+    header_dashboard()
+
+    st.markdown(
+        """
+        <style>
+
+            [data-testid="stAppViewContainer"] .block-container {
+                padding-top: 10px !important;
+                margin-top: -50px !important;
+            }
+        
+            div[data-testid="stHeadingWithActionElements"] {
+                margin-top: -1rem !important;
+                margin-bottom: -0.45rem !important;
+            }
+
+            div[data-testid="stHeadingWithActionElements"] + div[data-testid="stWidgetLabel"] {
+                margin-top: -0.4rem !important;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    st.header("Login using FaceID", text_alignment="center")
+    st.camera_input("Position your face in the center")
+
+    footer_dashboard()
+
