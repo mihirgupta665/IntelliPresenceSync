7141d2ac8521ecfc8af6a450327d5238d62faaaf
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-27 17:02:32 +0530

    teacher login function created which has the ability to match the entered password with the hased password of the database

--- src/database/db.py
+++ src/database/db.py
@@ -4,6 +4,10 @@ import bcrypt
 def hash_pass(pwd):
     return bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
 
+def check_pass(pwd, hashed):
+    return bcrypt.checkpw(pwd.encode(), hashed.encode())
+
+
 def check_teacher_exists(username):
     # Check  for unique username, returns true when username is already taken
     response = supabase.table("teachers").select("username").eq("username", username).execute(0)
@@ -15,4 +19,10 @@ def create_teacher(username, password, name):
     return response.data
 
 def teacher_login(username, password):
-    response = supabase.table("teachers").select("").eq("username", username).execute()
+    response = supabase.table("teachers").select("*").eq("username", username).execute()
+    if response.data: 
+        teacher = response.data[0]
+        if check_pass(password, teacher["password"]):
+            return teacher
+
+    return None
