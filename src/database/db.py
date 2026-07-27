7466230b99078d941f347cf4f0e72eac947b96b7
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-27 16:00:16 +0530

    function created to check the username exists or not to check for uniqueness of the same

--- src/database/db.py
+++ src/database/db.py
@@ -2,5 +2,9 @@ from src.database.config import supabase
 import bcrypt
 
 def check_teacher_exists(username):
-    # Check  for unique username, returns false when username is already taken
-    response = supabase.table("teachers").select("username")
+    # Check  for unique username, returns true when username is already taken
+    response = supabase.table("teachers").select("username").eq("username", username).execute(0)
+    return len(response.data)
+
+def create_teacher(username, password, name):
+    data = {"username" : username, "password" : hash_pass(password), "name" : name}
