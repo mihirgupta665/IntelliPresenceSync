7a96520e90711c090120c6e027c2ae853425faa0
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-27 10:50:38 +0530

    Login Page Created and structured and styled

--- src/components/footer.py
+++ src/components/footer.py
@@ -13,3 +13,17 @@ def footer_home():
         """,
         unsafe_allow_html=True,
     )
+
+
+def footer_dashboard():
+
+    st.markdown(
+        f"""
+            <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+                <p style="font-weight:bold; font-size:20px" > &copy; 2026 | Created by ❤️‍🔥 Mihir Gupta | All Rights Reserved</p>
+            </div>
+
+
+        """,
+        unsafe_allow_html=True,
+    )
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from src.components.footer import footer_dashboard
 
 from src.ui.base_layout import (
     style_background_dashboard,
@@ -19,21 +20,26 @@ def teacher_screen():
 
 
 def teacher_screen_login():
-    c1, c2 = st.columns(2, vertical_alignment="center", gap="xxlarge")
-    with c1:
-        header_dashboard()
-    with c2:
-        st.button("Go back to Home", type="secondary", key="loginbackbtn", shortcut="control+backspace")
+    header_dashboard()
+    
 
     st.header("Login using password", text_alignment="center")
+    st.space(size="small")
+
+    teacher_username = st.text_input("Enter Username", placeholder="Ex. Mihir Gupta" )
+    teacher_pass = st.text_input("Enter Password", type="password", placeholder="********")
+    st.divider()
+
+    btnc1, btnc2 = st.columns(2)
+
+    with btnc1:
+        st.button("Login", type="tertiary", icon=":material/passkey:", shortcut="control+enter", width="stretch")
+    with btnc2:
+        st.button("Register Instead", type="secondary", icon=":material/passkey:", width="stretch")
+
+
+    footer_dashboard()
 
 
 
-def teacher_screen_register():
-    c1, c2 = st.columns(2, vertical_alignment="center", gap="xxlarge")
-    with c1:
-        header_dashboard()
-    with c2:
-        st.button("Go back to Home", type="secondary", key="loginbackbtn", shortcut="control+backspace")
-
-    st.header("Login using password")
+       
