28f60dfe9f7eb23cba8e286aa46794805a88de00
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-28 13:26:09 +0530

    fucntion create dot get all the student data ot modeldefined to get the face then landmarks and then their corresponding embeddings

--- pipelines/face_pipeline.py
+++ pipelines/face_pipeline.py
@@ -1,2 +1,38 @@
 import dlib
 import numpy as np
+import face_recognition_models
+from sklearn.svm import SVC
+import streamlit as st
+
+from src.database.db import get_all_students
+
+
+@st.cache_resource  
+def load_dlib_models():
+    detector = dlib.get_frontal_face_detector()
+
+    sp = dlib.shape_predictor(
+        face_recognition_models.pose_predictor_model_location
+    )
+
+    facerec = dlib.face_recognition_model_v1(
+        face_recognition_models.face_recognition_model_location
+    )
+
+    return detector, sp, facerec
+
+
+def get_face_embeddings(image_np):
+    detector, sp, facerec = load_dlib_models()
+    faces = detector(image_np, 1) # 2
+
+    encodings = []
+
+    for face in faces:
+        shape = sp(image_np, face)
+        face_descriptor = facerec.compute_face_descriptor(image_np, shape, 1)
+
+        encodings.append(np.array(face_descriptor))
+
+    return encodings
+    
