e21ad66f86255c422342b7b3ebdb9c6687721f2a
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-30 00:49:21 +0530

    teacher attendance screen structurally created

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -67,21 +67,54 @@ def teacher_dashboard():
     tab1, tab2, tab3 = st.columns(3)
 
     with tab1:
-        if st.button("Take Attendance",width="stretch", icon=":material/ar_on_you:"):
+        type1 = "primary" if st.session_state.current_teacher_tab == "take_attendance" else "tertiary"
+        if st.button("Take Attendance", type=type1, width="stretch", icon=":material/ar_on_you:"):
             st.session_state.current_teacher_tab = "take_attendance"
             st.rerun()        
 
     with tab2:
-        if st.button("Manage Subjects",width="stretch", icon=":material/book_ribbon:"):
+        type2 = "primary" if st.session_state.current_teacher_tab == "manage_subjects" else "tertiary"
+        if st.button("Manage Subjects", type=type2, width="stretch", icon=":material/book_ribbon:"):
             st.session_state.current_teacher_tab = "manage_subjects"
             st.rerun()        
 
     with tab3:
-        if st.button("Attendance Histories", width="stretch", icon=":material/library_books:"):
-            st.session_state.current_teacher_tab = "attendance_record"
-            st.rerun()        
+        type3 = "primary" if st.session_state.current_teacher_tab == "attendance_records" else "tertiary"
+        if st.button("Attendance Records", type=type3, width="stretch", icon=":material/library_books:"):
+            st.session_state.current_teacher_tab = "attendance_records"
+            st.rerun()
+
+    st.divider()
+
+
+    if st.session_state.current_teacher_tab == "take_attendance":
+        teacher_tab_take_attendance()
+    if st.session_state.current_teacher_tab == "manage_subjects":
+        teacher_tab_manage_subjects()
+    if st.session_state.current_teacher_tab == "attendance_records":
+        teacher_tab_attendance_records()
+
 
     footer_dashboard()
+
+
+
+def teacher_tab_take_attendance():
+    st.header("AI Powered Attendance")
+    
+def teacher_tab_manage_subjects():
+    teacher_id = st.session_state.teacher_data["teacher_id"]
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("Subjects Management")
+    with col2:
+        if st.button("Create new Subject", width="stretch"):
+            create_subject_dialog(teacher_id)
+
+
+def teacher_tab_attendance_records():
+    st.header("Attendance Records")
+
 
 def login_teacher(username, password):
     if not username or not password:
