91ddf7ec72087170549b521b6f8a41eec1efd0ed
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-31 11:11:55 +0530

    database function created to implement the functionality to enrolll adn unenroll a student for a subject

--- src/database/db.py
+++ src/database/db.py
@@ -88,3 +88,20 @@ def get_teacher_subjects(teacher_id):
         sub.pop("attendance_logs", None)
 
     return subjects
+
+
+def enroll_student_to_subject(student_id, subject_id):
+    data = {
+        "student_id" : student_id,
+        "subject_id" : subject_id
+        }
+
+    response = execute_with_retries(supabase.table("subject_students").insert(data))
+    return response.data
+
+
+def unenroll_student_to_subject(student_id, subject_id):
+    response = execute_with_retries(supabase.table("subject_students").delete().eq("student_id", student_id).eq("subject_id", subject_id))
+    return response.data
+
+
